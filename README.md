6cf36e88c1d171139fa49bdc4236ed50e9d970c2
Naushad <naushadmahmood@gmail.com>
2015-08-28 09:30:49 +0200

    Basic project settup

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Financial-App
-Phybergate mentoring Finanacial-App, demonstrating the use of Spring with Thymeleaf and git for version Control, maven to build.
+Phybergate mentoring Finanacial-App, demonstrating the use of Spring-boot with Thymeleaf and git for version Control, maven(3.2+) to build.
 
 To use this project do the following:
 1. git clone https://github.com/Naushie/Financial-App.git
---
+++ src/main/java/za/co/naushad/ApplicationBoot.java
@@ -0,0 +1,15 @@
+package za.co.naushad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Naushad 
+ * This is the bootstrap class for the application.
+ */
+@SpringBootApplication
+public class ApplicationBoot {
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationBoot.class, args);
+	}
+}
